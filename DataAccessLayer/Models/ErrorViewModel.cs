f624b2921559a23ddb39cfacf85d787ecab5858f
DeathForRussia <ramirkiev@gmail.com>
2023-03-10 00:07:34 +0200

    joker

--- DataAccessLayer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DataAccessLayer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
